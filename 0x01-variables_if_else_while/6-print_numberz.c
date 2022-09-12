9564d2b678011a6884dc261666e6d798e9f2fa89
Rawllings <frawllings@example.com>
2022-09-12 10:44:07 -0700

    aphabeth

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: 0
+ *     * Prints base10 number with new line using put char
+ *      */
+int main(void)
+{
+		int value = 48;
+
+			while (value < 58)
+					{
+								putchar(value);
+										value++;
+											}
+				putchar('\n');
+					return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: 0
+ *     * Prints number with new line using put char
+ *      */
+int main(void)
+{
+		char alpha = 'z';
+
+			while (alpha >= 'a')
+					{
+								putchar(alpha--);
+									}
+
+				putchar('\n');
+
+					return (0);
+}
