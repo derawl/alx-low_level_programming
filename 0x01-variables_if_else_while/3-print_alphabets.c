06c5577914c6aed6c53e2e6644f2d37fd5a7178e
Rawllings <frawllings@example.com>
2022-09-12 10:20:25 -0700

    aphabeth

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0
+ *     */
+
+int main(void)
+{
+		char a = 'a';
+
+			while (a <= 'z')
+					{
+								putchar(a);
+										a++
+												}
+
+				a = 'A'
+
+							while (a <= 'z')
+									{
+												putchar(a);
+														a++
+																}
+
+					putchar('\n')
+
+								return (0);
+}
