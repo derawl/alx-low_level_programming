37244e59e0be038d2c2794999be3d6c4d1d88110
Rawllings <frawllings@example.com>
2022-09-12 10:46:37 -0700

    aphabeth

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: 0
+ *     */
+int main(void)
+{
+		int num;
+
+			for (num = 48; num < 58; num++)
+						putchar(num);
+				for (num = 'a'; num <= 'f'; num++)
+							putchar(num);
+					putchar('\n');
+						return (0);
+}
