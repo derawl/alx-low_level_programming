ed32cf2118cdaecd5e5f4d96da1a61f7f74d2003
Rawllings <frawllings@example.com>
2022-09-12 10:31:19 -0700

    aphabeth

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0
+ *     */
+
+int main(void)
+{
+		char a;
+
+			for (char b = 'a'; b <= 'z'; b++)
+					{
+								if(b != 'e' && b != 'q'){
+											    putchar(b);
+											    		}
+									}
+
+				putchar('\n');
+
+					return (0);
+}
