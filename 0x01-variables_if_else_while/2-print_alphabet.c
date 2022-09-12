c1c2224052a1576de147387a1e68908a1ae3c5b0
Rawllings <frawllings@example.com>
2022-09-12 03:26:02 -0700

    first commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *  
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char a = 'a';
+	while (a <= 'z'){							
+		putchar(a);
+		a++;
+	}
+	return (0);
+}
