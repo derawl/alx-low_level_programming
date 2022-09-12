377d85c88ed670b6c2c103eb40aa2a5e8f740f37
Rawllings <frawllings@example.com>
2022-09-12 10:35:19 -0700

    aphabeth

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: 0
+ *     */
+int main(void)
+{
+		int number;
+
+			for (number = 0; number < 10; number++)
+					{
+								printf("%i", number);
+									}
+				printf("\n");
+					return (0);
+}
