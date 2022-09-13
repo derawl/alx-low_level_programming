3d7fa1a5f49eb4839276c1a0116b98127b3cc855
Rawllings <frawllings@example.com>
2022-09-13 11:27:23 -0700

    first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet
+ *
+ * Return: 0
+*/
+void print_alphabet(void)
+{
+	char value;
+
+	for (value = 'a'; value <= 'z'; value++)
+	{
+		_putchar(value);
+	}
+_putchar('\n');
+}
