657f51c77a13f56b5a466fcbf1141a3578d9e812
Rawllings <frawllings@example.com>
2022-09-15 12:29:55 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_line - prints line n number of times
+*
+*Return: returns nothing
+*/
+
+void print_line(int n)
+{
+	
+    int n;
+
+    while(--n > 0)
+    {
+        _putchar('_');
+    }
+
+    _putchar('\n');
+}
