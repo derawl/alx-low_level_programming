117da39b6443699dce6593b63ed4d36cbbf22cb6
Rawllings <frawllings@example.com>
2022-09-15 12:18:34 -0700

    message

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,7 +9,7 @@ void print_numbers(void){
     int i;
 
     for(i = 48; i < 58; i++){
-        if(i != 50 && i != 52)
+        if((i != 50) || (i != 52))
         {
             _putchar(i);
         }
