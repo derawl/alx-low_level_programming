347316eaaf2e8aa79dc8e3a281515069d9116701
Rawllings <frawllings@example.com>
2022-09-15 12:31:43 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,19 +2,15 @@
 
 /**
 *print_line - prints line n number of times
-*
+*@n: parameter
 *Return: returns nothing
 */
 
 void print_line(int n)
 {
-	
-    int n;
-
     while(--n > 0)
     {
         _putchar('_');
     }
-
     _putchar('\n');
 }
