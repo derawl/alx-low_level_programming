4b841be12f104b07b4505ead29de79d64d5c4619
Rawllings <frawllings@example.com>
2022-09-20 12:29:30 -0700

    message

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,11 +3,12 @@
 /**
  *swap_int - swaps values in address
  *Return: returns nothing
-*/
+ */
 
 void swap_int(int *a, int *b)
 {
-	int c = *a;
-	int d = *b;
+	int c = &a;
+	int d = &b;
 	*a = d;
 	*b = c;
+}
