04eda791e381d475da4ba7170707be387f84efdc
Rawllings <frawllings@example.com>
2022-09-20 13:55:01 -0700

    message

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,24 +2,25 @@
 #include "main.h"
 
 /**
- * puts_half - prints a string
+ * rev_string - reverses a string
  * @str: string to print
  *
  * Return: nothing
  */
 
-void puts_half(char *str)
+void rev_string(char *str)
 {
-	int len = 0;
+	int len;
+	int i;
 
 	while (*(str + len) != '\0')
 	{
 		len++;
+		i++;
 	}
-
-	int i = len;
 
 	for (i = ((len - 1) / 2) + 1; i < len; i++)
 		putchar(*(str + i));
 	putchar(10);
 }
+
