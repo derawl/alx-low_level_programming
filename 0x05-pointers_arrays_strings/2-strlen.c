64278c2f25332cdbfa8a048718cb64b8835701f0
Rawllings <frawllings@example.com>
2022-09-20 13:44:45 -0700

    message

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- *swap_int - swaps values in address
- *@s: args s
- *Return: returns string length
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns length as integer;
  */
 
 int _strlen(char *s)
 {
-	int len;
-	while (*s != '\0')
-	{
+	int len = 0;
+
+	while (*(s + len) != '\0')
 		len++;
-		s++;
-	}
+
 	return (len);
 }
