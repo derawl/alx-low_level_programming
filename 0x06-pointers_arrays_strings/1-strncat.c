f6611cb6ee8d443a349a4e3bafd0295e9b0be88a
Rawllings <frawllings@example.com>
2022-09-21 13:42:43 -0700

    message

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,3 +1,6 @@
+#include "main.h"
+#include <stdio.h>
+
 /**                                                                                                                                  
  * _strncat - Concatenates two strings using at most                                                                                 
  *            an inputted number of bytes from src.                                                                                  
