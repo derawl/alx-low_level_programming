48337a6b079d50330d1c1f38f7d304ca1237f002
Rawllings <frawllings@example.com>
2022-09-21 13:27:21 -0700

    message

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,17 +6,15 @@
  *@src: str
  * Return: pointer to dest string.
 */
-char *_strcat(char *dest, char *src)
-{
-	int i = 0, len2 = 0;
-
-	while(dest[i++])
-		len2++;
-	
-
-	for(i = 0; src[i]; i++){
-		dest[len2++] = src[i]
-	}
-
-	return(dest);
+char *strcat(char *dest, const char *src)                                                                                            
+{                                                                                                                                    
+        int index = 0, dest_len = 0;                                                                                                 
+                                                                                                                                     
+        while (dest[index++])                                                                                                        
+                dest_len++;                                                                                                          
+                                                                                                                                     
+        for (index = 0; src[index]; index++)                                                                                         
+                dest[dest_len++] = src[index];                                                                                       
+                                                                                                                                     
+        return (dest);                                                                                                               
 }
