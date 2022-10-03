61281cf7035d8c4c8dc3cdabf26109601db04cc7
Rawllings <frawllings@example.com>
2022-10-03 11:34:56 -0700

    message

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - prints number of args sent to program
+ * @argc: number of args
+ * @argv: args
+ *
+ * Return: 0
+*/
+
+int main(int argc, char* argv[])
+{
+    printf("%d\n", argc);
+    return(0);
+}
