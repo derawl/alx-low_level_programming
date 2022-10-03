1eed642df2911d2f519054db49979955786453a9
Rawllings <frawllings@example.com>
2022-10-03 11:06:33 -0700

    message

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+/**
+ * main - prints name of program
+ * @argc: number of args
+ * @argv: args
+ *
+ * Return: 0
+*/
+
+#include<stdio.h>
+
+int main(int argc, char* argv[])
+{
+    printf("%s", argv[0]);
+    return 0;
+}
