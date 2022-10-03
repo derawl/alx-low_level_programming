fd09f86b21b6478bd3b35e8b3819c1cb57b5821b
Rawllings <frawllings@example.com>
2022-10-03 12:02:26 -0700

    message

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that prints its name, followed by a new line
+ * @argc: argument that counts argument input
+ * @argv: argument that stores the strings in an array of char* (strings)
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+    int num_1, num_2, result;
+    if(argc != 3){
+        printf("Error");
+        return(1);
+    }else{
+        num_1 = atoi(argv[0]);
+        num_2 = atoi(argv[1]);
+        result = num_1 * num_2;
+        printf("%d\n", result);
+    }
+    return(0);
+}
