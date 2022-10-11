85a827ca74b17228180ec4456d98c51faa112db6
Rawllings <frawllings@example.com>
2022-10-11 13:35:58 -0700

    message

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include "dog.h"
 /**
- *  init_dog - returns a struct of dog
- * @d: struct
- * @name: dog's name
- * @owner: dog owner
- * Return: pointer to the struct dog.
+ *init_dog - returns a struct of dog
+ *@d: struct
+ *@name: dog's name
+ *@owner: dog owner
+ * 
+ *Return: void.
 */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
@@ -15,5 +16,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
     d->name = name;
     d->age = age;
     d->owner = owner;
-    return (d);
 }
