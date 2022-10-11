fb7bafe0db4c3806872bca939d45659d37577e42
Rawllings <frawllings@example.com>
2022-10-11 13:22:19 -0700

    message

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef STRTYP
+#define STRTYP
 
 /**
 *main - header file for protoypes
@@ -12,9 +12,10 @@ struct dog
     float age;
     char *owner;
 };
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-struct dog dog_t;
-
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* MAIN_H */
