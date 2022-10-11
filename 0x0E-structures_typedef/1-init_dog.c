e184a45ae642cd1f8f9bb5b4560eb5214c100933
Rawllings <frawllings@example.com>
2022-10-11 13:38:04 -0700

    message

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,15 +5,15 @@
  *@d: struct
  *@name: dog's name
  *@owner: dog owner
- * 
+ *
  *Return: void.
 */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    if (d == 0)
-        return;
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+        if (d == 0)
+                return;
+        d->name = name;
+        d->age = age;
+        d->owner = owner;
 }
