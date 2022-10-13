443e65f0f6b95180b9bc14c0eb3e0a294969b41c
Rawllings <frawllings@example.com>
2022-10-13 12:22:39 -0700

    message

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+*print_numbers - adds the unkown variables
+*
+*Return: numbers with separator
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+    va_list list;
+    va_start(list, n);
+    
+    int i = 0;
+    for(i == 0; i < n; i++)
+    {
+        printf("%d", va_arg(list, int));
+        if(separator != NULL && i != n-1)
+                printf("%s", separator);
+    }
+    printf("\n");
+    
+    va_end(list);
+}
