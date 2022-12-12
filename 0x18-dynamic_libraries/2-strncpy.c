90ed7094197d24c6f676ac37593bed6904a52b66
Rawllings Brown <34872543+Jesuskid@users.noreply.github.com>
2022-12-12 19:38:47 +0100

    Update 2-strncpy.c

--- 0x18-dynamic_libraries/2-strncpy.c
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -10,15 +10,17 @@
  *                                                                                                                                   
  * Return: A pointer to the resulting string dest.                                                                                   
  */                                                                                                                                  
-char *_strncat(char *dest, char *src, int n)                                                                                         
-{                                                                                                                                    
-        int index = 0, dest_len = 0;                                                                                                 
-                                                                                                                                     
-        while (dest[index++])                                                                                                        
-                dest_len++;                                                                                                          
-                                                                                                                                     
-        for (index = 0; src[index] && index < n; index++)                                                                            
-                dest[dest_len++] = src[index];                                                                                       
-                                                                                                                                     
-        return (dest);                                                                                                               
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index;
+
+	for (index = 0; index < n && src[index] != '\0' ; index++)
+	{
+		dest[index] = src[index];
+	}
+	for (; index  < n; index++)
+	{
+		dest[index] = '\0';
+	}
+	return (dest);
 }
