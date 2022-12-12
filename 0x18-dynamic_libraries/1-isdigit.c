316bea060b3bf68b0f50c34e616bc717bf7f7344
Rawllings Brown <34872543+Jesuskid@users.noreply.github.com>
2022-12-12 19:28:05 +0100

    Create 1-isdigit.c

---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_isdigit - check the if character is digit or not.
+*@c: tested character
+*Return: 1 if is digit and zero if it isn't.
+*/
+
+int _isdigit(int c){
+
+    if((c >= 48) && (c <= 57))
+    {
+        return(1);
+    }
+    return 0;
+}
