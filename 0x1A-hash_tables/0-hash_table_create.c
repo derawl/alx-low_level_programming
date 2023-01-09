2025ec4df944218188822cd459a9afcf318c319e
Rawllings <frawllings@example.com>
2023-01-09 11:03:09 -0800

    message

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,6 +4,12 @@
 #include <stdbool.h>
 #include "hash_tables.h"
 
+/**
+ * hash_table_t - creates a new hash table in memory
+ * @size: size of the hash table
+ * Return: returns a hash table.
+ */
+
 hash_table_t *hash_table_create(unsigned long int size){
     hash_node_t nodes[size];
     hash_table_t hash_table = {.size=size, .array=nodes};
