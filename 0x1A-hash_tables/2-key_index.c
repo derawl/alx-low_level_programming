6def26832779473dd0356c01ba32e07a13144c95
Rawllings <frawllings@example.com>
2023-01-11 11:02:06 -0800

    message

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+able File  12 lines (11 sloc)  340 Bytes
+
+#include "hash_tables.h"
+
+/**
+ * key_index - Gives the index of a key
+ * @key: key of key/value pair
+ * @size: size of array of the hash table
+ * Return: index where key/value pair should be stored in array of hash table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
