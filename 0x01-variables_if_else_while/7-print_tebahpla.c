5454d442694ad1e765ce5f8b55a8bf13b4245c35
Rawllings Brown <34872543+Jesuskid@users.noreply.github.com>
2023-05-16 20:28:49 +0100

    Update 7-print_tebahpla.c
    betty

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: 0
- *     * Prints number with new line using put char
- *      */
+ * main - Entry point
+ *
+ * Return: 0
+ * Prints number with a new line using putchar
+*/
 int main(void)
 {
-		char alpha = 'z';
+	char alpha = 'z';
 
-			while (alpha >= 'a')
-					{
-								putchar(alpha--);
-									}
+	while (alpha >= 'a')
+	{
+		putchar(alpha--);
+	}
 
-				putchar('\n');
+	putchar('\n');
 
-					return (0);
+	return (0);
 }
