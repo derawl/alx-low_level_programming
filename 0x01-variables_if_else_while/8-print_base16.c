36e7c35d7fc48bc9f1e785d4083a879ff2c3b748
Rawllings Brown <34872543+Jesuskid@users.noreply.github.com>
2023-05-16 20:29:44 +0100

    Update 8-print_base16.c
    betty

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: 0
- *     */
+ * main - Entry point
+ *
+ * Return: 0
+*/
 int main(void)
 {
-		int num;
+	int num;
 
-			for (num = 48; num < 58; num++)
-						putchar(num);
-				for (num = 'a'; num <= 'f'; num++)
-							putchar(num);
-					putchar('\n');
-						return (0);
+	for (num = 48; num < 58; num++)
+		putchar(num);
+
+	for (num = 'a'; num <= 'f'; num++)
+		putchar(num);
+
+	putchar('\n');
+
+	return (0);
 }
