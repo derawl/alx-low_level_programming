600a534016809617c7f69351015d2cd961090394
Rawllings Brown <34872543+Jesuskid@users.noreply.github.com>
2023-05-16 20:35:16 +0100

    Update 100-print_comb3.c
    betty

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,36 +1,39 @@
 #include <stdio.h>
+
 /**
- *  * main - prints out all the numbers between 00 and 99
- *   * with no two digits being the same
- *    * Return: 0
- *     */
+ * main - prints out all the numbers between 00 and 99
+ * with no two digits being the same
+ *
+ * Return: 0
+*/
 int main(void)
 {
-		int i, j;
+	int i, j;
 
-			for (i = 48; i < 58; i++)
-					{
-								for (j = i; j < 58; j++)
-											{
-															if (i == j)
-																			{
-																								continue;
-																											}
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i; j < 58; j++)
+		{
+			if (i == j)
+			{
+				continue;
+			}
 
-																		putchar(i);
-																					putchar(j);
+			putchar(i);
+			putchar(j);
 
-																								if (i == 56 && j == 57)
-																												{
-																																	break;
-																																				}
-																											else
-																															{
-																																				putchar(',');
-																																								putchar(' ');
-																																											}
-																													}
-									}
-				putchar('\n');
-					return (0);
+			if (i == 56 && j == 57)
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
 }
