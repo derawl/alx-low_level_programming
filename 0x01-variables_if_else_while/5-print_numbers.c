2bf26e26d1e58323e4a48a3b250fe8996a493978
Rawllings Brown <34872543+Jesuskid@users.noreply.github.com>
2023-05-16 20:25:55 +0100

    Update 5-print_numbers.c
    betty

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: 0
- *     */
+ * main - Entry point
+ *
+ * Return: 0
+*/
 int main(void)
 {
-		int number;
+	int number;
 
-			for (number = 0; number < 10; number++)
-					{
-								printf("%i", number);
-									}
-				printf("\n");
-					return (0);
+	for (number = 0; number < 10; number++)
+	{
+		printf("%i", number);
+	}
+
+	printf("\n");
+	return (0);
 }
