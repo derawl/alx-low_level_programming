524382b0d15c24ad0c33335526cb7e3a75583312
Rawllings Brown <34872543+Jesuskid@users.noreply.github.com>
2023-05-16 20:26:39 +0100

    Update 6-print_numberz.c
    betty

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
+
 /**
- *  * main - Entry point
- *   *
- *    * Return: 0
- *     * Prints base10 number with new line using put char
- *      */
+ * main - Entry point
+ *
+ * Return: 0
+ * Prints base10 number with a new line using putchar
+*/
 int main(void)
 {
-		int value = 48;
+	int value = 48;
 
-			while (value < 58)
-					{
-								putchar(value);
-										value++;
-											}
-				putchar('\n');
-					return (0);
+	while (value < 58)
+	{
+		putchar(value);
+		value++;
+	}
+
+	putchar('\n');
+	return (0);
 }
